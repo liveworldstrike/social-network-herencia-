3dbca485eb276fa9712014fa5a1ac83c1137ebdf
liveworldstrike <lazodellamas12@gmail.com>
2016-05-13 12:22:47 +0200

    creada clase post y refactorizacion

--- MessagePost.java
+++ MessagePost.java
@@ -5,53 +5,17 @@ import java.util.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MessagePost
+public class MessagePost  extends Post
 {
-    private String userName;
     private String message;
-    private long timeStamp;
-    private int likes;
-    private ArrayList<String> comments;
 
     /**
      * Constructor for objects of class MessagePost
      */
     public MessagePost(String author, String text)
     {
-        userName = author;
+        super(author);
         message = text;
-        timeStamp =  System.currentTimeMillis();
-        likes = 0;
-        comments = new ArrayList<>();
-    }
-
-    /**
-     * suma un like si se le da
-     */
-    public void like()
-    {
-        likes++;
-    }
-
-    /**
-     * quita un like del contador , si este es menos de 0 lo iguala a 0 
-     */
-    public void unLike()
-    {
-        if (likes < 0){
-            likes = 0 ;
-        }
-        else{
-            likes--;
-        }
-    }
-
-    /**
-     * metodo para añadir comentarios
-     */
-    public void addComment(String text)
-    {
-        comments.add(text);
     }
 
     /**
@@ -62,48 +26,5 @@ public class MessagePost
         return message;
     }
 
-    /**
-     * el tiempo
-     */
-    public long getTimeStamp()
-    {
-        return timeStamp;
-    }
-
-    /**
-     * muestra todos los datos del post 
-     */
-    public void display()
-    {
-        System.out.println("User: " + userName);
-        System.out.println("Message: " + message);
-        System.out.println("Submitted :"  + (timeString(System.currentTimeMillis())));
-        System.out.println(likes + " Likes");
-        if(comments.size() == 0)
-        {
-            System.out.println("No comments");            
-        }
-        else
-        {
-            System.out.println("Comments : ");
-            int cont = 1;
-            for(String comment :comments){
-                System.out.println(cont  + ":" + comment);
-                cont++;
-            }
-        }
-    }
-
-    /**
-     * metodo para trasformar el tiempo 
-     */
-
-    public String timeString(long time)
-    {
-        long tiempoTrans = (time - timeStamp)/1000;
-        long min = (tiempoTrans /60);
-        long sec = tiempoTrans-(min*60);
-        return ("min " + min + "sec " +sec );
-    }
 }
 
